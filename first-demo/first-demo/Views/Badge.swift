a6cc86382a0d0f56c3df79e5b775e90b970d039d
kingcos <2821836721v@gmail.com>
2019-10-27 00:04:46 +0800

    Finshed forth

---
+++ first-demo/first-demo/Views/Badge.swift
@@ -0,0 +1,41 @@
+//
+//  Badge.swift
+//  first-demo
+//
+//  Created by kingcos on 2019/10/26.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geometry in
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
---
+++ first-demo/first-demo/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  first-demo
+//
+//  Created by kingcos on 2019/10/26.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
