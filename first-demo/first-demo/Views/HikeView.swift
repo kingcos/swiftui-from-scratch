a0a005f6e8e23991c2c85874a4ce2886bff8e0c9
kingcos <2821836721v@gmail.com>
2020-01-02 09:48:33 +0800

    Update

---
+++ first-demo/first-demo/Views/HikeView.swift
@@ -0,0 +1,51 @@
+//
+//  HikeView.swift
+//  first-demo
+//
+//  Created by kingcos on 2019/10/27.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(data: hike.observations, path: \.elevation)
+                    .frame(width: 50, height: 30)
+
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                }
+
+                Spacer()
+
+                Button(action: {
+                    self.showDetail.toggle()
+                }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .padding()
+                        .animation(.easeInOut())
+                }
+            }
+
+            if showDetail {
+                HikeDetail(hike: hike)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeView()
+    }
+}
