911ce7bda84586a93ec831ec70cffb86518ac592
kingcos <2821836721v@gmail.com>
2020-01-06 13:47:15 +0800

    Add converter demo

--- objccn-demos/01-Calculator/objccn-swiftui/ConverterDemo.swift
+++ objccn-demos/01-Calculator/objccn-swiftui/ConverterDemo.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+@propertyWrapper struct Converter {
+    let from: String
+    let to: String
+    let rate: Double
+    
+    var value: Double
+    
+    var wrappedValue: String {
+        get { "\(from) \(value)" }
+        set { value = Double(newValue) ?? -1 }
+    }
+        
+    var projectedValue: String {
+        return "\(to) \(value * rate)"
+    }
+    
+    init(
+        initialValue: String,
+        from: String,
+        to: String,
+        rate: Double
+    ) {
+        self.rate = rate
+        self.value = 0
+        self.from = from
+        self.to = to
+        self.wrappedValue = initialValue
+    }
+}
+
+struct Foo {
+    @Converter(initialValue: "100", from: "USD", to: "CNY", rate: 6.88)
+    var usd_cny
+
+    @Converter(initialValue: "100", from: "CNY", to: "EUR", rate: 0.13)
+    var cny_eur
+
+    func foo() {
+        // wrappedValue - projectedValue
+        print("\(usd_cny) = \($usd_cny)")
+        print("\(cny_eur) = \($cny_eur)")
+    }
+}
