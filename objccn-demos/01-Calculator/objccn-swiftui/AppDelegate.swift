9b7ced08a9ca42256a5db37cca54fe1cd9346d2d
kingcos <2821836721v@gmail.com>
2020-01-14 20:12:39 +0800

    Add comments

--- objccn-demos/01-Calculator/objccn-swiftui/AppDelegate.swift
+++ objccn-demos/01-Calculator/objccn-swiftui/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        Foo().foo()
+        
+        // Converter Demo
+        // Foo().foo()
+        
         return true
     }
 
