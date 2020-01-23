428139d104f7e00004c21e15aa6146c8215c6d33
kingcos <2821836721v@gmail.com>
2020-01-23 17:13:36 +0800

    Fixed settings

---
+++ design+code-demos/DesignCode/DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by kingcos on 2020/1/23.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
