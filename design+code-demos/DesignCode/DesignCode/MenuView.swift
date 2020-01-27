7a9a60a846d3e42ba90527691508ba8ee3f6b385
kingcos <2821836721v@gmail.com>
2020-01-27 16:40:50 +0800

    New menu view

---
+++ design+code-demos/DesignCode/DesignCode/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  DesignCode
+//
+//  Created by kingcos on 2020/1/27.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
