41ca99b99b73e9e5f1778002fed578b0953cc928
kingcos <2821836721v@gmail.com>
2020-01-29 01:15:22 +0800

    Add a scroll view

--- design+code-demos/DesignCode/DesignCode/HomeView.swift
+++ design+code-demos/DesignCode/DesignCode/HomeView.swift
@@ -22,7 +22,19 @@ struct HomeView: View {
                 AvatarView(showProfile: $showProfile)
             }
             .padding(.horizontal)
+            .padding(.leading, 30 - 16) // 与下面的 ScrollView 对齐（默认水平为 16）
             .padding(.top, 30)
+            
+            // 横向 ScrollView
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0 ..< 5) { item in
+                        SectionView()
+                    }
+                }
+                .padding(30)
+                .padding(.bottom, 30)
+            }
             
             Spacer()
         }
@@ -32,5 +44,34 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype designs in SwiftUI")
+                    .font(.system(size: 24, weight: .black))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(Color.white)
+                Spacer()
+                Image("Logo1")
+            }
+            
+            Text("18 Sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
