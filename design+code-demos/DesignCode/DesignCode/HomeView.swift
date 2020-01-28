d58bbe82fa48f402d88e25812c43f79c38551624
kingcos <2821836721v@gmail.com>
2020-01-29 01:56:40 +0800

    Show sheet view

--- design+code-demos/DesignCode/DesignCode/HomeView.swift
+++ design+code-demos/DesignCode/DesignCode/HomeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct HomeView: View {
     @Binding var showProfile: Bool
+    @State var showUpdate = false
     
     var body: some View {
         VStack {
@@ -20,6 +21,20 @@ struct HomeView: View {
                 
                 // 将 State 作为 Binding 传入
                 AvatarView(showProfile: $showProfile)
+                
+                Button(action: { self.showUpdate.toggle() }) {
+                    Image(systemName: "bell")
+                        .renderingMode(.original)
+                        .font(.system(size: 16, weight: .medium))
+                        .frame(width: 36, height: 36)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 1, y: 10)
+                }
+                .sheet(isPresented: $showUpdate) {
+                    ContentView()
+                }
             }
             .padding(.horizontal)
             .padding(.leading, 30 - 16) // 与下面的 ScrollView 对齐（默认水平为 16）
