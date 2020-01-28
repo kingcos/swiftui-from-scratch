710d1411d9eada52fb8c9f6ff9990c0fe562d3bf
kingcos <2821836721v@gmail.com>
2020-01-29 02:17:44 +0800

    Setup basic layout of navigation view

--- design+code-demos/DesignCode/DesignCode/UpdateList.swift
+++ design+code-demos/DesignCode/DesignCode/UpdateList.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  UpdateList.swift
 //  DesignCode
 //
@@ -10,7 +10,38 @@ import SwiftUI
 
 struct UpdateList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(updateData) { update in
+                NavigationLink(destination: Text(update.text)) {
+                    HStack {
+                        Image(update.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .background(Color.black)
+                            .cornerRadius(20)
+                            .padding(.trailing, 4)
+                        
+                        VStack(alignment: .leading, spacing: 8.0) {
+                            Text(update.title)
+                                .font(.system(size: 20, weight: .bold))
+                            
+                            Text(update.text)
+                                .lineLimit(2)
+                                .font(.subheadline)
+                                .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                            
+                            Text(update.date)
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+            .navigationBarTitle(Text("Updates"))
+        }
     }
 }
 
@@ -19,3 +50,39 @@ struct UpdateList_Previews: PreviewProvider {
         UpdateList()
     }
 }
+
+ struct Update: Identifiable {
+    var id = UUID()
+    var image: String
+    var title: String
+    var text: String
+    var date: String
+ }
+
+ let updateData = [
+     Update(image: "Card1",
+            title: "SwiftUI Advanced",
+            text: "Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.",
+            date: "JAN 1"),
+     
+     Update(image: "Card2",
+            title: "Webflow",
+            text: "Design and animate a high converting landing page with advanced interactions, payments and CMS",
+            date: "OCT 17"),
+     
+     Update(image: "Card3",
+            title: "ProtoPie",
+            text: "Quickly prototype advanced animations and interactions for mobile and Web.",
+            date: "AUG 27"),
+     
+     Update(image: "Card4",
+            title: "SwiftUI",
+            text: "Learn how to code custom UIs, animations, gestures and components in Xcode 11",
+            date: "JUNE 26"),
+     
+     Update(image: "Card5",
+            title: "Framer Playground",
+            text: "Create powerful animations and interactions with the Framer X code editor",
+            date: "JUN 11")
+ ]
+
