18fd0a4bbe2f736a2c0087b9156e8f2905531258
kingcos <2821836721v@gmail.com>
2020-01-30 01:44:01 +0800

    New course list view

---
+++ design+code-demos/DesignCode/DesignCode/CourseList.swift
@@ -0,0 +1,21 @@
+//
+//  CourseList.swift
+//  DesignCode
+//
+//  Created by kingcos on 2020/1/30.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+    }
+}
