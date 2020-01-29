6c85a3f4a6b22d49c4a62e01f2bd8fa609fa14ed
kingcos <2821836721v@gmail.com>
2020-01-30 01:59:11 +0800

    Setup course view

--- design+code-demos/DesignCode/DesignCode/CourseList.swift
+++ design+code-demos/DesignCode/DesignCode/CourseList.swift
@@ -10,12 +10,54 @@ import SwiftUI
 
 struct CourseList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            CourseView()
+        }
     }
 }
 
 struct CourseList_Previews: PreviewProvider {
     static var previews: some View {
         CourseList()
+    }
+}
+
+struct CourseView: View {
+    @State var show = false
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Text("SwiftUI Advanced")
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.white)
+                    Text("20 Sections")
+                        .foregroundColor(Color.white.opacity(0.7))
+                }
+                Spacer()
+                Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+            }
+            Spacer()
+            Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .frame(height: 140, alignment: .top)
+        }
+        .padding(show ? 30 : 20)
+        .padding(.top, show ? 30 : 0)
+//        .frame(width: show ? screen.width : screen.width - 60,
+//               height: show ? screen.height : 280)
+            .frame(maxWidth: show ? .infinity : screen.width - 60,
+                   maxHeight: show ? .infinity : 280)
+        .background(Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))) 
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), radius: 20, x: 0, y: 20)
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .onTapGesture {
+            self.show.toggle()
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
