b4f44bcf8d2771d5ddd773831369c3fdaf0faf74
kingcos <2821836721v@gmail.com>
2020-01-30 01:42:20 +0800

    Add view when if statement satisfied

--- design+code-demos/DesignCode/DesignCode/HomeView.swift
+++ design+code-demos/DesignCode/DesignCode/HomeView.swift
@@ -11,73 +11,96 @@ import SwiftUI
 struct HomeView: View {
     @Binding var showProfile: Bool
     @State var showUpdate = false
+    @Binding var showContent: Bool
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("Watching")
-//                    .font(.system(size: 28, weight: .bold))
-                    .modifier(CustomFontModifier())
-                
-                Spacer()
-                
-                // 将 State 作为 Binding 传入
-                AvatarView(showProfile: $showProfile)
-                
-                Button(action: { self.showUpdate.toggle() }) {
-                    Image(systemName: "bell")
-                        .renderingMode(.original)
-                        .font(.system(size: 16, weight: .medium))
-                        .frame(width: 36, height: 36)
-                        .background(Color.white)
-                        .clipShape(Circle())
-                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 1, y: 10)
-                }
-                .sheet(isPresented: $showUpdate) {
-                    UpdateList()
-                }
-            }
-            .padding(.horizontal)
-            .padding(.leading, 30 - 16) // 与下面的 ScrollView 对齐（默认水平为 16）
-            .padding(.top, 30)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                WatchRingsView()
-                    .padding(.horizontal, 30)
-                    .padding(.bottom, 30)
-            }
-            
-            // 横向 ScrollView
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20) {
-                    ForEach(sectionData) { item in
-                        GeometryReader { geometry in
-                            SectionView(section: item)
-                                // minX 会一直改变（左边 x 坐标），初始为 30
-                                .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -20),
-                                                  axis: (x: 0, y: 10, z: 0))
-                        }
-                        .frame(width: 275, height: 275)
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Watching")
+    //                    .font(.system(size: 28, weight: .bold))
+                        .modifier(CustomFontModifier())
+                    
+                    Spacer()
+                    
+                    // 将 State 作为 Binding 传入
+                    AvatarView(showProfile: $showProfile)
+                    
+                    Button(action: { self.showUpdate.toggle() }) {
+                        Image(systemName: "bell")
+                            .renderingMode(.original)
+                            .font(.system(size: 16, weight: .medium))
+                            .frame(width: 36, height: 36)
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 1, y: 10)
+                    }
+                    .sheet(isPresented: $showUpdate) {
+                        UpdateList()
                     }
                 }
-                .padding(30)
-                .padding(.bottom, 30)
+                .padding(.horizontal)
+                .padding(.leading, 30 - 16) // 与下面的 ScrollView 对齐（默认水平为 16）
+                .padding(.top, 30)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    WatchRingsView()
+                        .padding(.horizontal, 30)
+                        .padding(.bottom, 30)
+                        .onTapGesture {
+                            self.showContent = true
+                    }
+                }
+                
+                // 横向 ScrollView
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(sectionData) { item in
+                            GeometryReader { geometry in
+                                SectionView(section: item)
+                                    // minX 会一直改变（左边 x 坐标），初始为 30
+                                    .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -20),
+                                                      axis: (x: 0, y: 10, z: 0))
+                            }
+                            .frame(width: 275, height: 275)
+                        }
+                    }
+                    .padding(30)
+                    .padding(.bottom, 30)
+                }
+                .offset(y: -30)
+                
+                HStack {
+                    Text("Courses")
+                        .font(.title)
+                        .bold()
+                    Spacer()
+                }
+                .padding(.leading, 30)
+                .offset(y: -60)
+                
+                SectionView(section: sectionData[2],
+                            width: screen.width - 60,
+                            height: 275)
+                    .offset(y: -60)
+                
+                Spacer()
             }
-            
-            Spacer()
         }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(showProfile: .constant(false))
+        HomeView(showProfile: .constant(false), showContent: .constant(false))
     }
 }
 
 struct SectionView: View {
     var section: Section
+    var width: CGFloat = 275
+    var height: CGFloat = 275
     
     var body: some View {
         VStack {
@@ -100,7 +123,7 @@ struct SectionView: View {
         }
         .padding(.top, 20)
         .padding(.horizontal, 20)
-        .frame(width: 275, height: 275)
+        .frame(width: width, height: height)
         .background(section.color)
         .cornerRadius(30)
         .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
