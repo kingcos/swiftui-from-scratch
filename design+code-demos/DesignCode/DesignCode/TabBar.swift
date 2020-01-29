24ae9099f9cad3aaff4bb190953cffcd02572ad8
kingcos <2821836721v@gmail.com>
2020-01-29 15:14:29 +0800

    Setup tab bar view and as the first view of the app

--- design+code-demos/DesignCode/DesignCode/TabBar.swift
+++ design+code-demos/DesignCode/DesignCode/TabBar.swift
@@ -10,12 +10,25 @@ import SwiftUI
 
 struct TabBar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
-        TabBar()
+        Group {
+            TabBar().previewDevice("iPhone 8")
+            TabBar().previewDevice("iPhone SE")
+        }
     }
 }
