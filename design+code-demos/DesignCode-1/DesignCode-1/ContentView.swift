7ca3e0a7d7bd75d081a2e576a8b88bead4de4076
kingcos <2821836721v@gmail.com>
2020-01-31 01:54:39 +0800

    New design + code project

---
+++ design+code-demos/DesignCode-1/DesignCode-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/1/31.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
