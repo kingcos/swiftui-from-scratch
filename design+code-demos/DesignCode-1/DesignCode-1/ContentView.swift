9593aa6a85d71581e6c0c8ad6ef61c082f2e6a44
kingcos <2821836721v@gmail.com>
2020-01-31 22:35:22 +0800

    Add some gestures

--- design+code-demos/DesignCode-1/DesignCode-1/ContentView.swift
+++ design+code-demos/DesignCode-1/DesignCode-1/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var show = false
+    @State var viewState = CGSize.zero
      
     var body: some View {
         ZStack {
@@ -32,9 +33,10 @@ struct ContentView: View {
 //                                  axis: (x: 10.0, y: 10.0, z: 10.0))
                 .blendMode (.hardLight)
                 .animation(.easeInOut(duration: 0.7))
+                .offset(x: viewState.width, y: viewState.height)
             
             CardView()
-                .background(show ? Color.red : Color("background8"))
+                .background(show ? Color("background5") : Color("background8"))
                 .cornerRadius(10)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
@@ -44,8 +46,10 @@ struct ContentView: View {
 //                                  axis: (x: 10.0, y: 10.0, z: 10.0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.5))
+                .offset(x: viewState.width, y: viewState.height)
             
             CertificateView()
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
                 .rotationEffect(Angle(degrees: show ? 5 : 0 ))
 //                .rotation3DEffect(Angle(degrees: show ? 30 : 0),
@@ -54,6 +58,17 @@ struct ContentView: View {
                 .onTapGesture {
                     self.show.toggle()
                 }
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            self.viewState = value.translation
+                            self.show = true
+                        }
+                        .onEnded { value in
+                            self.viewState = CGSize.zero
+                            self.show = false
+                        }
+                )
         }
     }
 }
