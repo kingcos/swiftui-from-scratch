0a6e37c2a65e438cbad698ca55ee6e0548d231ad
kingcos <2821836721v@gmail.com>
2020-02-02 01:34:19 +0800

    Using SF symbols

--- design+code-demos/DesignCode-1/DesignCode-1/Home.swift
+++ design+code-demos/DesignCode-1/DesignCode-1/Home.swift
@@ -10,12 +10,42 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            MenuRow(image: "person.crop.circle", text: "My Account")
+            MenuRow(image: "creditcard", text: "Billing")
+            MenuRow(image: "person.and.person", text: "Team")
+            MenuRow(image: "arrow.uturn.down", text: "Sign out")
+            Spacer()
+        }
+        .padding(.top, 20)
+        .padding(30)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+struct MenuRow : View {
+    var image = "creditcard"
+    var text = "My Account"
+    
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .imageScale(.large)
+                .foregroundColor(Color.red)
+                .frame(width: 32, height: 32)
+            Text(text)
+                .font(.headline)
+            Spacer()
+        }
     }
 }
