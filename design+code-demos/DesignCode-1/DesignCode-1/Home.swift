38627fd8499bb529916b4430ba7074ba381dd771
kingcos <2821836721v@gmail.com>
2020-02-02 02:32:06 +0800

    Extract more details

--- design+code-demos/DesignCode-1/DesignCode-1/Home.swift
+++ design+code-demos/DesignCode-1/DesignCode-1/Home.swift
@@ -10,12 +10,28 @@ import SwiftUI
 
 struct Home: View {
     @State var show = false
+    @State var showProfile = false
     
     var body: some View {
         ZStack {
-            Button(action: { self.show.toggle() }) {
-                Text("Open Menu")
-            }
+            // Button(action: { self.show.toggle() }) {
+            // Text("Open Menu")
+            // }
+            
+            ContentView()
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 20)
+                .animation(.spring())
+                .offset(y: showProfile ? 0 : UIScreen.main.bounds.height)
+            
+            MenuButton(show: $show)
+                .offset(x: -30, y: showProfile ? 0 : 80)
+                .animation(.spring())
+            
+            MenuRight(show: $showProfile)
+                .offset(x: -16, y: showProfile ? 0 : 88)
+                .animation(.spring())
             
             MenuView(show: $show)
         }
@@ -87,5 +103,68 @@ struct MenuView : View {
             .onTapGesture {
                 self.show.toggle()
         }
+    }
+}
+
+struct MenuButton : View {
+    @Binding var show : Bool
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Button(action: { self.show.toggle() }) {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "list.dash")
+                            .foregroundColor(.black)
+                    }
+                    .padding(.trailing, 20)
+                    .frame(width: 90, height: 60)
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    .shadow(color: Color("buttonShadow"),
+                            radius: 10, x: 0, y: 10)
+                }
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct MenuRight : View {
+    @Binding var show : Bool
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                HStack {
+                    Button(action: { self.show.toggle() }) {
+                        CircleButton(icon: "person.crop.circle")
+                    }
+                    Button(action: { self.show.toggle() }) {
+                        CircleButton(icon: "bell")
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct CircleButton : View {
+    var icon = "person.crop.circle"
+    
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(.black)
+        }
+        .frame(width: 44, height: 44)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(color: Color("buttonShadow"),
+                radius: 10, x: 0, y: 10)
     }
 }
