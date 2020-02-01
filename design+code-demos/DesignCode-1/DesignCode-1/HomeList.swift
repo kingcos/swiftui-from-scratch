db824d8c1365b0c4bef0b2ae1351fd0a4ec81779
kingcos <2821836721v@gmail.com>
2020-02-02 02:33:11 +0800

    New home list

---
+++ design+code-demos/DesignCode-1/DesignCode-1/HomeList.swift
@@ -0,0 +1,21 @@
+//
+//  HomeList.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/2.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeList_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeList()
+    }
+}
