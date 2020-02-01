c78aca7aba9abac83ffdfbfc7a8b9e953a9f47bf
kingcos <2821836721v@gmail.com>
2020-02-02 01:31:45 +0800

    New home view

---
+++ design+code-demos/DesignCode-1/DesignCode-1/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/2.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
