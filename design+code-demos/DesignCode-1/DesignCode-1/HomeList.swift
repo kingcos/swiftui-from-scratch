56d11e87fe0251f248ff94e3afc331462ef24f57
kingcos <2821836721v@gmail.com>
2020-02-02 02:43:40 +0800

    Using scrollview

--- design+code-demos/DesignCode-1/DesignCode-1/HomeList.swift
+++ design+code-demos/DesignCode-1/DesignCode-1/HomeList.swift
@@ -10,12 +10,39 @@ import SwiftUI
 
 struct HomeList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0 ..< 3) { item in
+                    CourseView()
+                }
+            }
+        }
     }
 }
 
 struct HomeList_Previews: PreviewProvider {
     static var previews: some View {
         HomeList()
+    }
+}
+
+struct CourseView : View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Build an app with SwiftUI")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(20)
+                .lineLimit(4)
+                .frame(width: 150)
+            Spacer()
+            Image("Illustration1")
+        }
+        .background(Color("background3"))
+        .cornerRadius(30)
+        .frame(width: 246, height: 360)
+        .shadow(color: Color("backgroundShadow3"),
+                radius: 20, x: 0, y: 20)
     }
 }
