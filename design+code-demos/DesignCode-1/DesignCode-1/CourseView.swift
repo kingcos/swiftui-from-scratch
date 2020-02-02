cfebee00a6dc9ee21b5c41187615e6ba917018d6
kingcos <2821836721v@gmail.com>
2020-02-02 23:53:06 +0800

    New course view

---
+++ design+code-demos/DesignCode-1/DesignCode-1/CourseView.swift
@@ -0,0 +1,21 @@
+//
+//  CourseView.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/2.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseView_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseView()
+    }
+}
