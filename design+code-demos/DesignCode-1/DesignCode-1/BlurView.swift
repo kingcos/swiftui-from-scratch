a0d009f05d40d5a667025d56dc5f8718ba169086
kingcos <2821836721v@gmail.com>
2020-02-02 23:59:33 +0800

    New blur view

---
+++ design+code-demos/DesignCode-1/DesignCode-1/BlurView.swift
@@ -0,0 +1,21 @@
+//
+//  BlurView.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/2.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlurView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlurView()
+    }
+}
