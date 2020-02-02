f8f3ae65073225f2e61ad2be81b8df5435d75fb8
kingcos <2821836721v@gmail.com>
2020-02-03 02:18:47 +0800

    New tab bar

---
+++ design+code-demos/DesignCode-1/DesignCode-1/TabBar.swift
@@ -0,0 +1,21 @@
+//
+//  TabBar.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/3.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
