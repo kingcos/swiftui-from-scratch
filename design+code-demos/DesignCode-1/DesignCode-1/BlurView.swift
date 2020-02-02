61a2d95298e5893bf0cf6ff7db21a3544b5f2d05
kingcos <2821836721v@gmail.com>
2020-02-03 00:43:31 +0800

    Setup blur view

--- design+code-demos/DesignCode-1/DesignCode-1/BlurView.swift
+++ design+code-demos/DesignCode-1/DesignCode-1/BlurView.swift
@@ -8,14 +8,23 @@
 
 import SwiftUI
 
-struct BlurView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct BlurView: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+    
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+        ])
+        return view
     }
-}
-
-struct BlurView_Previews: PreviewProvider {
-    static var previews: some View {
-        BlurView()
-    }
+    
+    func updateUIView(_ uiView: UIView,
+                      context: UIViewRepresentableContext<BlurView>) {}
 }
