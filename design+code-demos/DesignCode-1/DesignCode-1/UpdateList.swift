2ecb5aca124a06aeb0c41f2be3c550758aa83e59
kingcos <2821836721v@gmail.com>
2020-02-03 01:03:14 +0800

    New update view

---
+++ design+code-demos/DesignCode-1/DesignCode-1/UpdateList.swift
@@ -0,0 +1,21 @@
+//
+//  UpdateList.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/3.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateList_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateList()
+    }
+}
