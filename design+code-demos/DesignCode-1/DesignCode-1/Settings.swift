7642a354919d60f7850ba8c0f453a56d0d01eb0d
kingcos <2821836721v@gmail.com>
2020-02-03 02:49:44 +0800

    New settings view

---
+++ design+code-demos/DesignCode-1/DesignCode-1/Settings.swift
@@ -0,0 +1,21 @@
+//
+//  Settings.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/3.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
