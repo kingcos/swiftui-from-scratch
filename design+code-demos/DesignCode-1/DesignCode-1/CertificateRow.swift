d159c6c9851addaf60bd5382d656d4943f46f9ef
kingcos <2821836721v@gmail.com>
2020-02-03 03:22:49 +0800

    New certificate row

---
+++ design+code-demos/DesignCode-1/DesignCode-1/CertificateRow.swift
@@ -0,0 +1,21 @@
+//
+//  CertificateRow.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/3.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CertificateRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CertificateRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CertificateRow()
+    }
+}
