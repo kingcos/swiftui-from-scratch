a2d7649ae31c3740b0a524ba0f7bcf902a31f0c0
kingcos <2821836721v@gmail.com>
2020-02-02 23:58:46 +0800

    New card bottom view

---
+++ design+code-demos/DesignCode-1/DesignCode-1/CardBottom.swift
@@ -0,0 +1,21 @@
+//
+//  CardBottom.swift
+//  DesignCode-1
+//
+//  Created by kingcos on 2020/2/2.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardBottom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardBottom_Previews: PreviewProvider {
+    static var previews: some View {
+        CardBottom()
+    }
+}
