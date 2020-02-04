48e061f9de95fa03917c69f40e1b6f2c8d126128
kingcos <2821836721v@gmail.com>
2020-02-04 16:53:20 +0800

    New project

---
+++ apple-demos/03-App Design and Layout/03-App Design and Layout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  03-App Design and Layout
+//
+//  Created by kingcos on 2020/2/4.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
