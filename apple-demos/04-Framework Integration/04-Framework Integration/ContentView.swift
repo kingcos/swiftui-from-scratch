dac91c3f97fd8fbbb1649ae43e5723751e021c9a
kingcos <2821836721v@gmail.com>
2020-02-06 01:53:43 +0800

    New project

---
+++ apple-demos/04-Framework Integration/04-Framework Integration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  04-Framework Integration
+//
+//  Created by kingcos on 2020/2/6.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
