50443d97394d99a0920cd9dd3a09761275b43b3b
kingcos <2821836721v@gmail.com>
2020-02-15 01:32:54 +0800

    New course detail view

---
+++ design+code-demos/02-DesignCode/DesignCode/CourseDetail.swift
@@ -0,0 +1,21 @@
+//
+//  CourseDetail.swift
+//  DesignCode
+//
+//  Created by kingcos on 2020/2/15.
+//  Copyright © 2020 kingcos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CourseDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseDetail()
+    }
+}
