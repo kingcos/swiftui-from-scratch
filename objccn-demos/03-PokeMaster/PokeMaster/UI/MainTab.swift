8da8a028fec7d245d922aef0679f6c9256b92aa1
kingcos <2821836721v@gmail.com>
2020-04-13 00:39:45 +0800

    Using TabView

---
+++ objccn-demos/03-PokeMaster/PokeMaster/UI/MainTab.swift
@@ -0,0 +1,33 @@
+//
+//  MainTab.swift
+//  PokeMaster
+//
+//  Created by kingcos on 2020/4/12.
+//  Copyright © 2020 OneV's Den. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainTab: View {
+    var body: some View {
+        TabView {
+            PokemonRootView().tabItem {
+                // tabItem 只接受 Image 和 Text
+                Image(systemName: "list.bullet.below.rectangle")
+                Text("列表")
+            }
+            
+            SettingRootView().tabItem {
+                Image(systemName: "gear")
+                Text("设置")
+            }
+        }
+//        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct MainTab_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTab()
+    }
+}
