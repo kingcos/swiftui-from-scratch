cf1a854fc5070505725076eb1650acf16989acbf
kingcos <2821836721v@gmail.com>
2020-04-26 02:56:44 +0800

    Support URL scheme

--- objccn-demos/03-PokeMaster/PokeMaster/SceneDelegate.swift
+++ objccn-demos/03-PokeMaster/PokeMaster/SceneDelegate.swift
@@ -12,52 +12,67 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Create the SwiftUI view that provides the window contents.
-        // 内部子控件谁需要 environmentObject 谁声明即可自动注入
-        let contentView = MainTab().environmentObject(Store())
-
-        // Use a UIHostingController as window root view controller.
+    
+    private func showMainTab(scene: UIScene,
+                             with store: Store) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = UIHostingController(rootView: MainTab().environmentObject(store))
             self.window = window
             window.makeKeyAndVisible()
         }
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    
+    // pokemaster://showPanel?id={id}
+    private func createStore(_ URLContexts: Set<UIOpenURLContext>) -> Store {
+        let store = Store()
+        
+        guard let url = URLContexts.first?.url,
+            let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { return store }
+        
+        switch (components.scheme, components.host) {
+        case ("pokemaster", "showPanel"):
+            guard let idQuery = (components.queryItems?.first { $0.name == "id" }),
+                  let idString = idQuery.value,
+                  let id = Int(idString) else { break }
+            
+            store.appState.pokemonList.selectionState = .init(expandingIndex: id,
+                                                              panelIndex: id,
+                                                              panelPresented: true)
+        default:
+            break
+        }
+        
+        return store
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        let store = createStore(connectionOptions.urlContexts)
+        showMainTab(scene: scene, with: store)
+        
+//        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+//        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+//
+//        // Create the SwiftUI view that provides the window contents.
+//        // 内部子控件谁需要 environmentObject 谁声明即可自动注入
+//        let contentView = MainTab().environmentObject(Store())
+//
+//        // Use a UIHostingController as window root view controller.
+//        if let windowScene = scene as? UIWindowScene {
+//            let window = UIWindow(windowScene: windowScene)
+//            window.rootViewController = UIHostingController(rootView: contentView)
+//            self.window = window
+//            window.makeKeyAndVisible()
+//        }
     }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
+    
+    func scene(_ scene: UIScene,
+               openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        let store = createStore(URLContexts)
+        showMainTab(scene: scene, with: store)
     }
 
 
